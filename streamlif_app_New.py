4bec0422f83fd2df72f0dfe6d60a9c6018097e1f
Sonia GACI <113120486+sogaci@users.noreply.github.com>
2022-09-11 14:30:11 +0200

    Update streamlif_app_New.py

--- streamlif_app_New.py
+++ streamlif_app_New.py
@@ -9,6 +9,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑 Test OK')
 
 
 #import requests
